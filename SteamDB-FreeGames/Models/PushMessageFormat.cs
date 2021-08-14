a7a3c3255615ad4c05b54fe26d4630eb85e04ab4
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2021-08-15 02:00:48 +0800

    Move format strings. Categorize models.

---
+++ SteamDB-FreeGames/Models/PushMessageFormat.cs
@@ -0,0 +1,23 @@
+﻿namespace SteamDB_FreeGames.Models {
+	public class PushMessageFormat {
+		public static readonly string telegramPushFormat =
+			"<b>{0}</b>\n\n" +
+			"Sub/App ID: <i>{1}</i>\n" +
+			"免费类型: {2}\n" +
+			"链接: <a href=\"{3}\" > {4}</a>\n" +
+			"开始时间: {5}\n" +
+			"结束时间: {6}\n";
+
+		public static readonly string barkPushFormat =
+			"{0}\nSub/App ID: {1}\n免费类型: {2}\n" +
+			"链接: {3}\n开始时间: {4}\n结束时间: {5}";
+
+		public static readonly string emailPushHtmlFormat =
+			"<p><b>{0}</b><br>" +
+			"Sub/App ID: {1}<br>" +
+			"免费类型: {2}<br>" +
+			"链接: <a href=\"{3}\" > {4}</a><br>" +
+			"开始时间: {5}<br>" +
+			"结束时间: {6}</p>";
+	}
+}
---
+++ SteamDB-FreeGames/Models/Record/FreeGameRecord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace SteamDB_FreeGames.Models {
+	public class FreeGameRecord {
+		public string ID { get; set; }
+		public string Url { get; set; }
+	
+		public string Name { get; set; }
+
+		public string FreeType { get; set; }
+		public DateTime StartTime { get; set; }
+
+		public DateTime EndTime { get; set; }
+
+		public string ToTelegramMessage() {
+			return new StringBuilder().AppendFormat(PushMessageFormat.telegramPushFormat, Name, ID, FreeType, Url, Name, StartTime.ToString(), EndTime.ToString()).ToString();
+		}
+
+		public string ToBarkMessage() {
+			return new StringBuilder().AppendFormat(PushMessageFormat.barkPushFormat, Name, ID, FreeType, Url, StartTime.ToString(), EndTime.ToString(), ID).ToString();
+		}
+
+		public string ToEmailMessage() {
+			return new StringBuilder().AppendFormat(PushMessageFormat.emailPushHtmlFormat, Name, ID, FreeType, Url, Name, StartTime.ToString(), EndTime.ToString()).ToString();
+		}
+	}
+}
